d4fe888e75a67911306f8d34f11e6155cd4cf937
Thoan2396 <Thoan2396@gmail.com>
2025-08-10 02:18:30 +0900

    Upload web

---
+++ js/group01.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.slider01').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+  });
+});
+$(document).ready(function(){
+  $('.slider08').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 3000,
+   arrows: false,
+  });
+});
